13f4780bd4548cd81af4d10bcc97c8ceaa283eac
haeji <haejipark88@gmail.com>
2022-08-27 21:36:54 +0900

    프로젝트 생성

---
+++ Common/app/src/main/java/com/haeji/common/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.haeji.common
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.haeji.common.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
