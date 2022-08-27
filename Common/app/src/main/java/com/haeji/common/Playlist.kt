43197324584db1257e2aabbb6834cb45d2f51994
minyeong <ninja2319@naver.com>
2022-08-28 03:05:13 +0900

    플레이리스트

--- Common/app/src/main/java/com/haeji/common/Playlist.kt
+++ Common/app/src/main/java/com/haeji/common/Playlist.kt
@@ -3,16 +3,36 @@ package com.haeji.common
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class Playlist : AppCompatActivity() {
+    lateinit var playlistRvAdapter: PlaylistRvAdapter
+    val datas = mutableListOf<AlbumData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playlist)
 
-
+        initRecycler()
     }
 
+    private fun initRecycler(){
+        playlistRvAdapter = PlaylistRvAdapter(this)
+        val playListRV = findViewById<RecyclerView>(R.id.playlistRV)
+        playListRV.adapter = playlistRvAdapter
 
+        datas.apply {
+            add(AlbumData(img = R.drawable.album, title = "타인의 기억", singer = "넬"))
+            add(AlbumData(img = R.drawable.album, title = "summer", singer = "tvt"))
+            add(AlbumData(img = R.drawable.album, title = "머물러줘", singer = "성규"))
+
+
+            playlistRvAdapter.datas = datas
+            playlistRvAdapter.notifyDataSetChanged()
+            
+
+        }
+        Log.d("데이터",datas.toString())
+    }
 }
