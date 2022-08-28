c9268b3fbc500c1868b5eaa10034474692496da1
minyeong <ninja2319@naver.com>
2022-08-28 15:50:37 +0900

    메인 인텐트 연결

--- Common/app/src/main/java/com/haeji/common/MainActivity.kt
+++ Common/app/src/main/java/com/haeji/common/MainActivity.kt
@@ -11,11 +11,30 @@ class MainActivity : AppCompatActivity() {
         
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
+        // 채팅
         binding.chatBtn.setOnClickListener {
-            val intent = Intent(this, ChattingRoom::class.java)
+            val intent = Intent(this, Chatting::class.java)
+            startActivity(intent)
+
+        }
+        // 마이페이지
+        binding.imageView.setOnClickListener {
+            val intent = Intent(this, MyPage::class.java)
+            startActivity(intent)
+
+        }
+        // 퀴즈
+        binding.quizImg.setOnClickListener {
+                val intent = Intent(this, QuizList::class.java)
+            startActivity(intent)
+
+        }
+
+        // 테스트
+
+        // 플레이리스트
+        binding.XplaylistImg.setOnClickListener {
+            val intent = Intent(this, Playlist::class.java)
             startActivity(intent)
 
         }
