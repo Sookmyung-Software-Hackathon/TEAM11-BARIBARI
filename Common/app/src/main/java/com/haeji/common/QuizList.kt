ef0cf8d0ce80b2f7a6a7ddcedf53e54f557dd72d
JoSujin-cando <81570268+JoSujin-cando@users.noreply.github.com>
2022-08-28 18:18:03 +0900

    제대로 된 이동은 아님.
    퀴즈리스트에서 퀴즤 선택 시 임시 이동

--- Common/app/src/main/java/com/haeji/common/QuizList.kt
+++ Common/app/src/main/java/com/haeji/common/QuizList.kt
@@ -1,11 +1,31 @@
 package com.haeji.common
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.haeji.common.databinding.ActivityMainBinding
+import com.haeji.common.databinding.ActivityQuizListBinding
 
 class QuizList : AppCompatActivity() {
+
+    val binding by lazy{ActivityQuizListBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_list)
+        setContentView(binding.root)
+        //음악퀴즈
+        binding.musicBtn.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java)
+        }
+        //영화퀴즈
+        binding.movieBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+        }
+        //단어퀴즈
+        binding.wordBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+        }
     }
 }
